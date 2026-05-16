e9a618fba43d8c4bf83b571c0c05fdb542d0351a
wwhclavier-svg <wwhclavier@gmail.com>
2026-05-17 06:35:13 +0800

    chore: add verify scripts for Box/Penta1L/SR212-3m and archive old bub00_k8 relation

---
+++ verify/Box/Compare-CPPMeta-Box.m
@@ -0,0 +1,31 @@
+(* C++ Verify Metadata Export *)
+$CPPMeta = <|
+  "Family" -> "Box",
+  "TotalTime" -> 6.372878,
+  "Modulus" -> 179424673,
+  "Regions" -> {
+    <| "RegionIndex" -> 1, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 2, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 3, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 4, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 5, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 6, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 7, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 8, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 9, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 10, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 11, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 12, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>,
+    <| "RegionIndex" -> 13, "NE" -> 4, "NB" -> 2, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 910 |>,
+    <| "RegionIndex" -> 14, "NE" -> 4, "NB" -> 2, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 910 |>,
+    <| "RegionIndex" -> 15, "NE" -> 4, "NB" -> 1, "NIBP" -> 4, "Incre" -> 2, "Nimax" -> 455 |>
+  },
+  "Coefficients" -> {
+    { (* k=0 *)
+1
+    },
+    { (* k=1 *)
+14952056*v1 + 14952056*v2 + 44856167*v3 + 59210142*v4 + 89712336*v1^2 + 1*v1*v2 + 179424671*v1*v3 + 71769870*v1*v4 + 89712336*v2^2 + 2*v2*v3 + 107654803*v2*v4 + 89712334*v3^2 + 11*v3*v4 + 104066310*v4^2
+    }
+  }
+|>
---
+++ verify/SR212-3m/Compare-CPPMeta-SR212-3m.m
@@ -0,0 +1,24 @@
+(* C++ Verify Metadata Export *)
+$CPPMeta = <|
+  "Family" -> "SR212-3m",
+  "TotalTime" -> 31.015143,
+  "Modulus" -> 179424673,
+  "Regions" -> {
+    <| "RegionIndex" -> 1, "NE" -> 5, "NB" -> 1, "NIBP" -> 6, "Incre" -> 2, "Nimax" -> 1820 |>,
+    <| "RegionIndex" -> 2, "NE" -> 5, "NB" -> 1, "NIBP" -> 6, "Incre" -> 2, "Nimax" -> 1820 |>,
+    <| "RegionIndex" -> 3, "NE" -> 5, "NB" -> 1, "NIBP" -> 6, "Incre" -> 2, "Nimax" -> 1820 |>,
+    <| "RegionIndex" -> 4, "NE" -> 5, "NB" -> 1, "NIBP" -> 6, "Incre" -> 2, "Nimax" -> 1820 |>,
+    <| "RegionIndex" -> 5, "NE" -> 5, "NB" -> 1, "NIBP" -> 6, "Incre" -> 2, "Nimax" -> 1820 |>,
+    <| "RegionIndex" -> 6, "NE" -> 5, "NB" -> 1, "NIBP" -> 6, "Incre" -> 2, "Nimax" -> 1820 |>,
+    <| "RegionIndex" -> 7, "NE" -> 5, "NB" -> 1, "NIBP" -> 6, "Incre" -> 2, "Nimax" -> 1820 |>,
+    <| "RegionIndex" -> 8, "NE" -> 5, "NB" -> 1, "NIBP" -> 6, "Incre" -> 2, "Nimax" -> 1820 |>
+  },
+  "Coefficients" -> {
+    { (* k=0 *)
+1
+    },
+    { (* k=1 *)
+12816048*v1 + 111072416*v2 + 12816048*v3 + 111072416*v4 + 128160469*v5 + 119616448*v2^2 + 179424669*v2*v5 + 119616448*v4^2 + 179424669*v4*v5 + 179424661*v5^2
+    }
+  }
+|>
